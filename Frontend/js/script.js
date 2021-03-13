d89a4de4f603d622bd28a851d5aee22568f5cf99
collinskoech11 <collinskoechck34@gmail.com>
2021-03-13 14:17:14 +0300

    update

---
+++ Frontend/js/script.js
@@ -0,0 +1,11 @@
+document.getElementById('expand').addEventListener('click',
+    function(){
+        var fill = document.querySelector('.form');
+        fill.classList.toggle('show');
+    });
+
+    document.getElementById('expand1').addEventListener('click',
+    function(){
+        var fill = document.querySelector('.form1');
+        fill.classList.toggle('show');
+    });
