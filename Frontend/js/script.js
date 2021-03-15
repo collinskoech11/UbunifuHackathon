1f20a794b03a35b13721de382b1cb81e94b2c435
misheckgithongo <misheckgithongo@gmail.com>
2021-03-15 22:09:43 +0300

    send alert done

--- Frontend/js/script.js
+++ Frontend/js/script.js
@@ -1,11 +1,30 @@
-document.getElementById('expand').addEventListener('click',
-    function(){
-        var fill = document.querySelector('.form');
-        fill.classList.toggle('show');
-    });
+// document.getElementById('expand').addEventListener('click',
+//     function(){
+//         var fill = document.querySelector('.form');
+//         fill.classList.toggle('show');
+//     });
 
-    document.getElementById('expand1').addEventListener('click',
-    function(){
-        var fill = document.querySelector('.form1');
-        fill.classList.toggle('show');
-    });
+//     document.getElementById('expand1').addEventListener('click',
+//     function(){
+//         var fill = document.querySelector('.form1');
+//         fill.classList.toggle('show');
+//     });
+
+document.getElementById('alert').addEventListener('click', ()=>{
+    console.log('send request')
+    fetch(`https://alert-system-farmers.herokuapp.com/api/v1/alert`,{
+        method: 'POST',
+        headers:{
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify({ 
+            farmers: [],
+        })})
+        .then(res=> res.json())
+        .then((res) => {
+            console.log(res)
+        })
+        .catch((error)=>{
+            console.log(error)
+        })
+})
